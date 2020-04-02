cfbafe11c53cd9836ec8c9d34699354bc1897efc
ryotokuro <ryotokuro@gmail.com>
2020-04-02 23:55:56 +1100

    moved websites to section 1, added new directory for js

---
+++ 2. javascript/queue.js
@@ -0,0 +1,12 @@
+function nextInLine(arr, item) {
+  arr.push(item);
+  return item =   arr.shift();
+}
+
+// Setup
+var testArr = [1,2,3,4,5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
